7a4b5e1c6f7d2b7885a44547c3c6c246a4cd5932
Zepesky <zepeskyutn@gmail.com>
2026-04-01 20:16:02 -0300

    Se agrego del ej7 al ej10 (entradas y salidas).

---
+++ entrada_y_salidas/ej10.py
@@ -0,0 +1,31 @@
+#10. Realizar un programa que a partir del ingreso del importe de una compra, aplique un 25% de descuento. Mostrar por pantalla cuánto es el total a pagar y cuánto fue el descuento obtenido.
+
+print("""
+__________________
+    apusmarket
+==================
+    Productos 
+==================
+1. Monster: $2,900
+2. Cafe: $2,000
+3. Pizza: $ 12,000
+==================
+""")
+
+
+while True:
+    
+    x = int(input("Ingrese el importe del producto: "))
+    y = str(input("Desea comprar algo mas? (s/n): "))
+    a = int(input("Ingrese el importe del producto: "))
+    
+    if y != "s":
+        
+        productos = x + a
+
+        total = productos
+
+        print(f"{total}")
+        
+        break
+
